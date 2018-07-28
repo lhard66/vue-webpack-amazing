1631b8db79eea20ce21668290d76130bb89c90d7
Allen Liu <lhard@qq.com>
2018-07-28 22:18:54 +0800

    双向绑定示例

---
+++ src/views/model/ModelDemo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <span>{{person.name}}</span>
+    <input type="text" v-model="person.age"></input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'example',
+  props: {
+    value: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      person: {
+        name: '',
+        age: '888',
+      },
+    }
+  },
+  mounted() {
+    this.setPerson(this.value);
+  },
+  methods: {
+    setPerson(p) {
+      // 做一层判断，防止传入null时报错
+      if (p) {
+        // 确保value和person指向不同的内容地址
+        this.person = p;
+      }
+    },
+  },
+  watch: {
+    person: {
+      handler(val, oldVal) {
+        // console.log(val);
+        this.$emit('input', val);
+      },
+      deep: true,
+    },
+  },
+}
+</script>
---
+++ src/views/model/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <!-- <span>name: {{outPerson.name}}</span> -->
+    <span>---</span>
+    <!-- <span>age: {{outPerson.age}}</span> -->
+    <hr>
+    <model-demo v-model="outPerson"></model-demo>
+    <button @click="confirm">改变</button>
+  </div>
+</template>
+
+<script>
+import ModelDemo from './ModelDemo.vue';
+
+export default {
+  name: 'use-example',
+  components: {
+    ModelDemo,
+  },
+  data() {
+    // 设置person初始值
+    return {
+      outPerson: {
+        name: 'jim',
+        age: 18,
+      },
+      // outPerson: null,
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      // 模拟异步请求数据p
+      const p = {
+        name: 'lucy',
+        age: 22,
+      };
+      this.outPerson.name = p.name;
+      this.outPerson.age = p.age;
+      console.log('over');
+    }, 2000);
+  },
+  methods: {
+    confirm() {
+      // 复制一份outPerson复本，改变复本的name，防止outPerson的name改变。
+      let newPerson = Object.assign({}, this.outPerson);
+      newPerson.name = 'ltq';
+      console.log(newPerson);
+    },
+  }
+}
+</script>
