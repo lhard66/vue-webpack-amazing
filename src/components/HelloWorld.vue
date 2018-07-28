b63f53694053310f819bf4a94025d66ca2880f88
Allen Liu <lhard@qq.com>
2018-07-28 22:31:11 +0800

    优化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ demo }}</h1>
     <h1>{{ demo2 }}</h1>
   </div>
@@ -24,25 +24,7 @@ export default {
     demo2() {
       return join(['a', 'b', 'c'], '~');
       // return '123';
-    }
+    },
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
