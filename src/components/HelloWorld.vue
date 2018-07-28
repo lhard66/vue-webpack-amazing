13947a5dfb133ad9582aa9a9becdafc03c6ba019
Allen Liu <lhard@qq.com>
2018-07-28 18:30:14 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ demo }}</h1>
+    <h1>{{ demo2 }}</h1>
+  </div>
+</template>
+
+<script>
+import { concat, join } from 'lodash';
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    demo() {
+      var array = [1];
+      return concat(array, 2, [3], [[4]]);
+    },
+    demo2() {
+      return join(['a', 'b', 'c'], '~');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
